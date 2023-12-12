d61ff4541f59bf34a2d9d366a5615be47392840c
Rafal Salwa <rafal@salwa.com.pl>
2023-12-12 23:25:36 +0100

    added symfony ux-turbo, ux-autocomplete, ux-react

--- src/Entity/TechTags.php
+++ src/Entity/TechTags.php
@@ -13,45 +13,8 @@ class TechTags
     #[ORM\Column]
     private ?int $id = null;
 
-    #[ORM\Column(length: 255)]
-    private ?string $name = null;
-
-    #[ORM\Column(length: 255)]
-    private ?string $tag = null;
-
     public function getId(): ?int
     {
         return $this->id;
-    }
-
-    public function setId(int $id): static
-    {
-        $this->id = $id;
-
-        return $this;
-    }
-
-    public function getName(): ?string
-    {
-        return $this->name;
-    }
-
-    public function setName(string $name): static
-    {
-        $this->name = $name;
-
-        return $this;
-    }
-
-    public function getTag(): ?string
-    {
-        return $this->tag;
-    }
-
-    public function setTag(string $tag): static
-    {
-        $this->tag = $tag;
-
-        return $this;
     }
 }
